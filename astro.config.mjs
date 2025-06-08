600839a0f229479d61aab95c2cb64a467deb1b7d
AlexanderAKM <a.k.muller.2@student.rug.nl>
2025-06-08 21:34:51 +0200

    trying math template

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,22 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://alexanderakm.github.io',   // 👈 exact URL
   integrations: [
     tailwind(),
-    mdx(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
     sitemap(),
   ],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'github-dark',
       wrap: true,
